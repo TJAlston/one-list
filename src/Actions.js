2cea5c56b734f74c7a079b0d02b6777384229cde
TJ Alston <tjalston82@yahoo.com>
2016-09-14 18:27:20 -0400

    compared to class notes

--- src/Actions.js
+++ src/Actions.js
@@ -1,20 +1,20 @@
   export function newListItem(text) {
     return {
-      type: 'NEW_ITEM'
-      text:
+      type: 'NEW_ITEM',
+      item: {text, complete:false}
     }
   }
 
   export function completeListItem(index) {
     return {
-      type: 'COMPLETE_ITEM'
+      type: 'COMPLETE_ITEM',
       index:
     }
   }
 
   export function removeListItem(index) {
     return {
-      type: 'REMOVE_ITEM'
+      type: 'REMOVE_ITEM',
       index:
     }
   }
