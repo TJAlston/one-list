85872d3a7bd44693422a34d93afc473bc6f5dda3
TJ Alston <tjalston82@yahoo.com>
2016-09-13 22:47:28 -0400

    figuring out imports

---
+++ src/Store.js
@@ -0,0 +1,10 @@
+import { createStore, compse} from 'redux'
+import { syncHistoryWithStore } from 'react-router-redux'
+import {browserHistory} from 'react-router'
+
+
+import rootReducer from './reducers/index'
+
+import App from './App'
+
+const store = createStore(rootReducer, defaultState)
